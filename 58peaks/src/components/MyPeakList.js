9c8c63f0b1a620aba7f386ceb2bb690739aab763
David <Davidmaxsands@gmail.com>
2022-02-16 09:13:32 -0700

    updated my peaks

--- 58peaks/src/components/MyPeakList.js
+++ 58peaks/src/components/MyPeakList.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Map from './Map'
+import MyPeak from './MyPeak'
 
 function MyPeakList() {
+const [myPeaks, setMyPeaks] = useState([])
+
+useEffect(() => {
+  fetch('http://localhost:3002/peaks')
+  .then(res => res.json())
+  .then(data => setMyPeaks(data))
+}, [])
+
   return (
-    <div>
+    <div id='my-peaks-container'>
+      <ul className='my-peaks'>
+        {myPeaks.map(peak => (
+          <MyPeak 
+          name={peak.name}
+          elevation={peak.elevation}
+          mtnClass={peak.class}
+          range={peak.range}
+          myTime={peak.myTime}
+          myImage={peak.myImage}
+          myRoute={peak.myRoute}
+          comment={peak.myComment}
+          />
+        ))}
+      </ul>
       <Map />
     </div>
   )
